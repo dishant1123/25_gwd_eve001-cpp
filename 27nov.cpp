ce59b80cd8d5f0bf5d4d9c2763cd7382d5a9ec3c
dishant1123 <dishantshah1417@gmail.com>
2025-11-27 18:47:28 +0530

    ....

---
+++ 27nov.cpp
@@ -0,0 +1,39 @@
+// print  hello in cpp 
+/*
+#include<iostream>
+int main()
+{
+    std ::cout<<"hello"<<std::endl;
+    std::cout<<"ram";
+    return 0; 
+}
+    */ 
+
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"hello"<<endl;
+    cout<<"ram";
+    return 0;
+}
+*/ 
+
+/*
+int a@ =10   ==> not 
+int 12a =90   ==> not 
+int _=78  ==> correct 
+int a12=90   ==> correct
+
+1. variable  ==> start digit , special character   ==> except :_ 
+2. last digit  is valid 
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int __a=90 ;
+    cout<<"value is  :"<<__a; 
+    return 0;
+}
