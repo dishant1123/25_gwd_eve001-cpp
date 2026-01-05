5c20522b2ee8b1af977981d7daed4b391f028ea1
dishant1123 <dishantshah1417@gmail.com>
2026-01-05 15:32:04 +0530

    ....

--- yash_tejas_backup.cpp
+++ yash_tejas_backup.cpp
@@ -45,7 +45,7 @@ data type  :
 5. bool : true or false
 6. double : decimal pos or neg 
 */
-
+/*
 #include<iostream>
 using namespace std;
 int main()
@@ -68,3 +68,45 @@ int main()
     cout<<"f value is : "<<f<<endl;
     return 0; 
 }
+*/ 
+
+// operator  : 
+/*
+1. aithematic : + - * / % 
+    ex :  1 %10   =  1  2 %10 =2       
+2. relational : == != > < >= <=
+    ex :  float a =10 , float b=10.0  (a==b)
+3. assignment : = += -= *= /= %=
+4. logical : && ||    
+5. bitwise : & | ^ << >>
+6. increment : post  inc : i++  post dec i --  pre inc ++i  pre dec --i
+    ex : int a =10 ; 
+        a++ + --a  + ++a  + a--  + a++  + --a   == > ?
+        10  + 10 + 11  + 11  + 10  + 10 
+  
+        int  b=20 
+        --b + ++b  - b-- + b++  - --b  + ++b   
+        19 + 20 - 20 +19 - 18 + 19  
+7.tenary : ? :
+*/
+
+/*
+conditional statement :
+
+if  ( con )            if(con)
+{                      {
+    cout<<                  cout<<
+}                      } 
+else                   else if (con)
+{                            cout<<
+    cout<<             else 
+}                            cout<< 
+*/
+
+/*
+task:1 ask user to enter the  3 number  and  check which one  is  big  small  medium
+
+input  : a=10 b=20 c=89    
+    output  : c big  b mediam  a small 
+ 
+*/
