a04acd92cb413d846271643222292cc9f66b590d
dishant1123 <dishantshah1417@gmail.com>
2026-03-23 17:46:15 +0530

    ....

--- oops/20march.cpp
+++ oops/20march.cpp
@@ -107,3 +107,157 @@ class cart
             return items;
         }
 };
+class User 
+{
+    string  username ="user"; 
+    string  password = "123"; 
+
+    public : 
+        bool login()
+        {
+            string u,p;
+            cout<<"\nEnter username : ";
+            cin>>u;
+            cout<<"\nEnter password : ";
+            cin>>p;
+
+            return (u==username && p==password);
+        }
+};
+
+class Admin 
+{
+    string  username ="admin";
+    string  password = "admin";
+
+    public : 
+        bool login()
+        {
+            string u,p;
+            cout<<"\nEnter username : ";
+            cin>>u;
+            cout<<"\nEnter password : ";
+            cin>>p;
+            return (u==username && p==password);
+        }
+    void addproduct(vector<Product*> &products)
+    {
+        int  id; 
+        string name; 
+        int  price; 
+        int type; 
+
+        cout<<"1.electonics"<<endl;
+        cout<<"2.clothes"<<endl;
+        cin>>type;
+        cout<<"\nEnter id : name : price : ";
+        cin>>id>>name>>price;
+
+        if(type==1)
+        {
+            products.push_back(new electonics(id, name, price));
+
+        }
+        else 
+        {
+            products.push_back(new clothes(id, name, price));
+        }
+        cout<<"\nProduct added"<<endl;
+
+    }
+    void remove_product(vector<Product*> &products,int id)
+    {
+        for(int i=0;i<products.size();i++)
+        {
+            if(products[i]->get_id()==id)
+            {
+                products.erase(products.begin()+i);
+                cout<<"\nProduct removed"<<endl;
+                return;
+            }
+        }
+        cout<<"\nProduct not found"<<endl;
+    }
+};
+void searchproduct(vector<Product*> &products,string key)
+{
+    for(auto p:products )
+    {
+        if(p->get_name()==key)
+        {
+            p->display();
+        }
+    }
+}
+
+void saveorder(cart &c)
+{
+    ofstream file("orders.txt",ios::app); 
+    for(auto &i :c.get_items())
+    {
+        file<<i.product->get_name()<<endl;
+        file<<i.qty<<endl;
+        file<<i.product->get_price()<<endl; 
+    }
+    file<<"total : "<<c.get_total()<<endl;
+    file<<'------------------------------\n'; 
+    file.close();
+
+}
+int  main()
+{
+    vector<Product*> products;
+
+    products.push_back(new electonics(1, "mouse", 100));
+    products.push_back(new clothes(2, "shirt", 200));
+
+    User user; 
+    Admin admin;
+    cart c; 
+
+    int choice ; 
+    cout<<"1. login"<<endl;
+    cout<<"2.admin - login"<<endl;
+    if(choice ==1)
+    {
+        if(!user.login())
+        {
+            cout<<"\nLogin failed"<<endl;
+            return 0; 
+        }
+    }
+    else if (choice ==2)
+    {
+        if(admin.login())
+        {
+            int ch; 
+            do{
+                cout<<"\n1.add product"<<endl;
+                cout<<"2.remove product"<<endl;
+                cout<<"3.exit"<<endl;
+                cin>>ch; 
+                if(ch==1)
+                {
+                    admin.addproduct(products);
+                }
+                else if (ch ==2)
+                {
+                    int id; 
+                    cout<<"Enter id : ";
+                    cin>>id;
+                    admin.remove_product(products,id);
+                }
+
+            }while(ch!=3);
+        }
+    }
+    return 0;
+    
+}
+
+
+/*
+read  : exiting file  ==> txt   
+write : new create + write  ==> exiting file  ==> overwrite  
+append : new create + write  ==> exiting file  ==> add 
+*/
