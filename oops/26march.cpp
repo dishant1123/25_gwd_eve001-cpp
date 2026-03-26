7de3a40905761e988c84fca36a27140d033c963e
dishant1123 <dishantshah1417@gmail.com>
2026-03-26 19:39:21 +0530

    ....

--- oops/26march.cpp
+++ oops/26march.cpp
@@ -62,7 +62,7 @@ int main()
 */ 
 
 // ex :3 pop_back : 
-
+/*
 #include <iostream>
 #include<vector>
 using namespace std;
@@ -91,5 +91,73 @@ int  main()
     // {
     //     cout<<x<<endl;
     // }
+    return 0; 
+}
+*/
+/*
+task  :1  
+
+1. create empty vector  
+2. 5 element  added ==> using push_back
+3. index =3 update  
+4. print the vector
+5. pop_back
+6 .after pop_back print 
+*/
+
+// ex :4  pointer with vector : 
+/*
+#include <iostream>
+#include<vector>
+using namespace std;
+int  main()
+{
+    vector<int>v={1,2,3};// empty vector
+
+    // int  a =10 ;  // int *p =&a   // int a[5] = {1,2,3,4,5}  *(p+i)
+
+    vector<int>*p = &v; 
+
+    for(int i=0; i<v.size(); i++)
+    {
+        cout<<(*p)[i]<<endl;  // v[i]
+    }
+    return 0; 
+}
+*/ 
+
+// ex :5  vector using class and object :
+
+#include <iostream>
+#include<vector>
+using namespace std;
+class student 
+{
+    public : 
+        int id; 
+        string  name; 
+    student(int i, string n)
+    {
+        id =i;
+        name =n;
+
+    }
+    void show()
+    {
+        cout<<"id : "<<id<<" name : "<<name<<endl;
+    }
+};
+int  main()
+{
+    vector<student>v;  // student s[3]; 
+    v.push_back(student(1,"john"));
+    v.push_back(student(2,"trisha"));
+    v.push_back(student(3,"sneha"));
+    v.push_back(student(4,"om"));
+
+    for(int i=0; i<v.size(); i++)
+    {
+        v[i].show();
+    }
     return 0; 
 }
